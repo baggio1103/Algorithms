6c1fd1150c93c9ec0d16a66de9172eb03f7cecf4
baggio1103 <46907001+baggio1103@users.noreply.github.com>
2019-10-03 13:33:14 +0500

    Create SortingAlgorithms.java

---
+++ SortingAlgorithms.java
@@ -0,0 +1,33 @@
+public class SortLevel1 {
+
+    public static boolean BubbleSortStep(int[] array){
+        boolean swapped = false;
+        int temp;
+        for (int i = 0; i < array.length-1; i++){
+            if (array[i] > array[i+1]){
+                temp = array[i];
+                array[i] = array[i+1];
+                array[i+1] = temp;
+                swapped = true;
+            }
+        }
+        return swapped;
+    }
+
+    public static void SelectionSortStep(int[] array, int i){
+        if (i+1 < array.length){
+            int index = i+1;
+            int min = array[index];
+            for (int j = i+1; j < array.length; j++){
+                if (min > array[j]){
+                    min = array[j];
+                    index = j;
+                }
+            }
+            min = array[i];
+            array[i] = array[index];
+            array[index] = min;
+        }
+    }
+
+}
