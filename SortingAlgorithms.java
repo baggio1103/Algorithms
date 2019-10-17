47df11d4f485b948db47d40627d5b7c5af289717
baggio1103 <46907001+baggio1103@users.noreply.github.com>
2019-10-17 23:45:27 +0300

    Update SortingAlgorithms.java

--- SortingAlgorithms.java
+++ SortingAlgorithms.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 
 public class SortLevel {
-
     public static boolean BubbleSortStep(int[] array){
         boolean swapped = true;
         int temp;
@@ -20,7 +19,7 @@ public class SortLevel {
         ArrayList<Integer> list = KnuthSequence(array.length);
         for (int i = 0; i < list.size(); i++){
             for (int k = 0; k < list.get(i); k++){
-                InsertionSortStep(array,list.get(i),k);
+                InsertionSortStep(array, list.get(i),k);
             }
         }
     }
@@ -40,7 +39,24 @@ public class SortLevel {
             array[index] = min;
         }
     }
-    
+
+//    public static void InsertionSortStep(int[] array, int step, int i) {
+//        int temp;
+//        int index = i;
+//        for (int j = i; j < array.length; j += step) {
+//            if (array[index] > array[j]) {
+//                temp = array[j];
+//                for (int k = j; k >= i; k -= step) {
+//                    if (k - step >= i) {
+//                        array[k] = array[k - step];
+//                    }
+//                }
+//                array[i] = temp;
+//            }
+//            index = j;
+//        }
+//    }
+
     public static void InsertionSortStep(int[] array, int step, int i) {
         int temp;
         int index = i;
@@ -67,8 +83,25 @@ public class SortLevel {
             index = j;
         }
     }
-    
-    public static ArrayList<Integer> KnuthSequence(int array_size){
+
+    public static void sort(int[] arr){
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            /* Move elements of arr[0..i-1], that are
+               greater than key, to one position ahead
+               of their current position */
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static ArrayList<Integer> KnuthSequence(int array_size) {
         ArrayList<Integer> list = new ArrayList<>();
         int i = 0;
         while ((i = 3 * i + 1) <= array_size) {
@@ -91,38 +124,44 @@ public class SortLevel {
             return 3*sequence(iterator-1)+1;
         }
     }
-    
+
     public static int ArrayChunk(int[] array){
         int i = 0;
         int j = array.length-1;
-        int base = array.length/2;
+        int index = array.length/2;
+        int frame = array[index];
         int temp;
+
         while (true){
-            while (array[i] < array[base]){
+            while (array[i] < frame){
                 i++;
             }
-            while (array[j] > array[base]){
+            while (array[j] > frame){
                 j--;
             }
-            if ((i == (j - 1)) && (array[i] < array[j])){
+            if ((i == (j - 1)) && (array[i] > array[j])){
                 temp = array[i];
                 array[i] = array[j];
                 array[j] = temp;
-                if (array[i] == array[base]){
-                    base = i;
+                if (array[i] == frame){
+                    index = i;
                 }else {
-                    base = j;
+                    index = j;
                 }
                 i = 0;
                 j = array.length/2;
             } else if ((i == j) || ((array[i] < array[j]) && i == j - 1)){
-                return base;
+                return index;
             }else {
                 temp = array[i];
                 array[i] = array[j];
                 array[j] = temp;
+                if (array[i] == frame){
+                    index = i;
+                }else if (array[j] == frame){
+                    index = j;
+                }
             }
         }
     }
-    
 }
