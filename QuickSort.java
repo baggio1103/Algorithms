9cf75974a94330c8c756bd3055653a03163fb2b0
baggio1103 <46907001+baggio1103@users.noreply.github.com>
2019-11-17 11:27:19 +0300

    Update QuickSort.java

--- QuickSort.java
+++ QuickSort.java
@@ -21,53 +21,6 @@ public class SortLevel {
             }
         }
   }
-
-
-    public static int ArrayChunk(int[] array, int left, int right){
-        int i = left;
-        int j = right;
-        int index = (left+right)/2;
-        int frame = array[index];
-        int temp;
-        boolean goTo = false;
-        while (true) {
-            if (goTo) {
-                i = left;
-                j = right;
-                index = (left+right)/2;
-                frame = array[index];
-                goTo = false;
-            }
-            while (array[i] < frame) {
-                i++;
-            }
-            while (array[j] > frame) {
-                j--;
-            }
-            if ((i == (j - 1)) && (array[i] > array[j])) {
-                temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
-                if (array[i] == frame) {
-                    index = i;
-                } else if (array[j] == frame) {
-                    index = j;
-                }
-                goTo = true;
-            } else if ((i == j) || ((array[i] < array[j]) && i == j - 1)) {
-                return index;
-            } else {
-                temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
-                if (array[i] == frame) {
-                    index = i;
-                } else if (array[j] == frame) {
-                    index = j;
-                }
-            }
-        }
-    }
  
   public static ArrayList<Integer> KthOrderStatisticsStep(int[] Array, int L, int R, int k ){
      ArrayList<Integer> list = new ArrayList<>();
