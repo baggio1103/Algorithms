e836b8b7c1d5415ba925851d674b526a2d1bc1b2
baggio1103 <46907001+baggio1103@users.noreply.github.com>
2019-12-01 22:04:42 +0300

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -14,22 +14,24 @@ public class BinarySearch {
 
     public void Step(int N){
         int middle = (Right + Left) / 2;
-        if (found == 0) {
-            if (Left == Right && array[middle] != N) {
-                found = -1;
-            }
-            if (array[middle] == N) {
-                found = 1;
-            } else if (array[middle] > N) {
-                Right = middle - 1;
-            } else if (array[middle] < N) {
-                Left = middle + 1;
-            }
+        if (Left > Right){
+            found = -1;
+            return;
         }
+
+        if (array[middle] == N) {
+            found = 1;
+            return;
+        } else if (array[middle] > N) {
+            Right = middle - 1;
+        } else if (array[middle] < N) {
+            Left = middle + 1;
+        }
+        Step(N);
     }
 
     public int GetResult(){
         return found;
     }
-
+    
 }
