0646e5c5a948943b31c928961ba8e6707cc6f01c
baggio1103 <46907001+baggio1103@users.noreply.github.com>
2019-12-02 17:46:37 +0300

    Update PartialBinarySearch.java

--- PartialBinarySearch.java
+++ PartialBinarySearch.java
@@ -11,19 +11,21 @@ public class BinarySearch {
         this.Right = array.length-1;
         found = 0;
     }
-
+    
     public void Step(int N){
         int middle = (Right + Left) / 2;
         if (found == 0){
             if (Left > Right){
                 found = -1;
-            }
-
-            if (array[middle] > N){
+            }else if(array[middle] == N) {
+                found = 1;
+            }else if (array[middle] > N){
                 Right = middle - 1;
             }else if (array[middle] < N){
                 Left = middle + 1;
-            }else if(array[middle] == N) {
+            }
+
+            if (Left == Right){
                 found = 1;
             }
         }
@@ -32,5 +34,7 @@ public class BinarySearch {
     public int GetResult(){
         return found;
     }
-
+    
 }
+
+
