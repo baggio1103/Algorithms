80606e5aa3b7c95b208d1cec55e4b31d224d0a56
baggio1103 <46907001+baggio1103@users.noreply.github.com>
2019-12-04 00:09:56 +0300

    Update PartialBinarySearch.java

--- PartialBinarySearch.java
+++ PartialBinarySearch.java
@@ -10,7 +10,7 @@ public class BinarySearch {
         this.Right = array.length-1;
         found = 0;
     }
-    
+
     public void Step(int N){
         int middle = (Right + Left) / 2;
         if (found == 0){
@@ -29,6 +29,35 @@ public class BinarySearch {
                 found = -1;
             }
         }
+    }
+
+    public boolean GallopingSearch(int[] array, int N){
+        int i = 1;
+        int index = (int) (Math.pow(2,i) - 2);
+
+        while (index < array.length){
+            if (array[index] == N){
+                found = 1;
+                return true;
+            }else if (array[index] < N){
+                i++;
+                index = (int) (Math.pow(2, i) - 2);
+                if (index >= array.length){
+                    Left = (int) (1 + Math.pow(2, (i-1)) - 2);
+                    Right = array.length-1;
+                }
+            }else if (array[index] > N){
+                index = array.length;
+                Left = (int) (1 + Math.pow(2, (i-1)) - 2);
+                Right = index;
+            }
+        }
+
+        while (found == 0){
+            Step(N);
+        }
+
+        return found == 1;
     }
 
     public int GetResult(){
