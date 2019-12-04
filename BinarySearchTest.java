6ba7a5c885568a05a0c7e3e19838bbbaefc02798
baggio1103 <46907001+baggio1103@users.noreply.github.com>
2019-12-04 18:43:22 +0300

    Update BinarySearchTest.java

--- BinarySearchTest.java
+++ BinarySearchTest.java
@@ -6,11 +6,23 @@ public class BinarySearchTest {
     @Test
     public void gallopingSearch() {
         int[] array = new int[99];
-        for (int i = 0; i < 99; i++){
-            array[i] = i+1;
+        for (int i = 0; i < 99; i++) {
+            array[i] = i + 1;
         }
         BinarySearch binary = new BinarySearch(array);
         assertTrue(binary.GallopingSearch(array, 49));
         assertEquals(1, binary.GetResult());
     }
+
+    @Test
+    public void test(){
+        int[] array = new int[99];
+        for (int i = 0; i < 99; i++) {
+            array[i] = i + 1;
+        }
+        BinarySearch binary = new BinarySearch(array);
+        assertFalse(binary.GallopingSearch(array, -49));
+        assertEquals(-1, binary.GetResult());
+    }
+
 }
